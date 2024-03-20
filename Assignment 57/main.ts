815376dd764ae278bbb65d28e0b6e062aafd180a
nishanazar <zuhaibsoomro.sst@hotmail.com>
2024-03-20 23:02:34 +0500

    2nd commit

---
+++ Assignment 57/main.ts
@@ -0,0 +1,11 @@
+// A list of grades
+let grades = [88, 94, 72, 99, 53, 77];
+
+// Calculates the average grade
+let averageGrade =
+  grades.reduce((total, grade) => total + grade, 0) / grades.length;
+
+// Shows the average grade
+console.log(averageGrade);
+
+//First, we add up all the grades. then, we divide by how many grades there are to get the average.

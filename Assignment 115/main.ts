3a67b79e99680cf54483c00c43a67c577598652f
nishanazar <zuhaibsoomro.sst@hotmail.com>
2024-04-09 13:20:05 +0500

    22th commit

---
+++ Assignment 115/main.ts
@@ -0,0 +1,31 @@
+// This function logs the day of the week based on a number
+
+function logDayOfWeek(dayNumber: number): void {
+  switch (dayNumber) {
+    case 1:
+      console.log("Sunday");
+      break;
+    case 2:
+      console.log("Monday");
+      break;
+    case 3:
+      console.log("Tuesday");
+      break;
+    case 4:
+      console.log("Wednesday");
+      break;
+    case 5:
+      console.log("Thrusday");
+      break;
+    case 6:
+      console.log("Friday");
+      break;
+    case 7:
+      console.log("Saturday");
+      break;
+    default:
+      console.log("Invalid day number");
+      break;
+  }
+}
+console.log(3); // outputs : Tuesday
